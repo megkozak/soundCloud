1bba975ba52c6b5ab764658dd962ef129782a9c0
Meg Kozak <megkozak@gmail.com>
2017-03-30 18:35:46 -0400

    latest changes

--- js/main.js
+++ js/main.js
@@ -1,5 +1,29 @@
-$("button").click(
-  function(){
-    alert('imclicked!');
-  }
+$("#play").click(
+    function() {
+        // alert('imclicked!');
+        // var audioElement = $('audio');
+        // console.log(audioElement);
+        document.getElementById('player').play();
+
+    }
 );
+$("#pause").click(
+    function() {
+        // alert('imclicked!');
+        // var audioElement = $('audio');
+        // console.log(audioElement);
+        document.getElementById('player').pause();
+        var obj = document.createElement('audio');
+        obj.controls = true;
+
+    }
+);
+//
+//
+//     }
+// );
+// $(document).ready(function() {
+//     var audioElement = document.createElement('audio');
+//     audioElement.setAttribute('src', 'mp3/DpMfP6qUSBo.mp3');
+// }
+// );
