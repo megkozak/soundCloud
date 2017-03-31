08cdaa61e003c537fd65c6465da4f131f80aa2c6
Meg Kozak <megkozak@gmail.com>
2017-03-30 20:44:06 -0400

    trying to make it play multiple songs

--- js/main.js
+++ js/main.js
@@ -1,25 +1,31 @@
 $("#play").click(
     function() {
-        // alert('imclicked!');
-        // var audioElement = $('audio');
-        // console.log(audioElement);
         document.getElementById('player').play();
 
     }
 );
 $("#pause").click(
     function() {
-        // alert('imclicked!');
-        // var audioElement = $('audio');
-        // console.log(audioElement);
         document.getElementById('player').pause();
-        var obj = document.createElement('audio');
-        obj.controls = true;
+
 
     }
 );
 //
+$("#nextSong").click(
+  function() {
+    document.getElementById('player').src = '/Users/megankozak/Documents/homework/soundCloud/mp3/Imagine Dragons - Believer (Audio) copy.mp3';
+    document.getElementById('player').play();
+    var songs = new Array("")
+  }
+
+);
+
 //
+
+
+
+
 //     }
 // );
 // $(document).ready(function() {
@@ -27,3 +33,24 @@ $("#pause").click(
 //     audioElement.setAttribute('src', 'mp3/DpMfP6qUSBo.mp3');
 // }
 // );
+// function track (name,controls,src,volume,loop) {
+//
+//        var  audio = document.createElement('audio');
+//
+//      audio.name = name;
+//      audio.controls = controls;
+//      audio.src = src;
+//      audio.volume = volume;
+//      audio.loop = loop;
+//
+//     return audio;
+//
+// };
+//
+// var music = [
+// track('AudioGood',true,'sounds/good.wav',0.5,false),
+// track('AudioWrong',true,'sounds/wrong.wav',0.5,false),
+// track('musicBackground',true,'sounds/background.ogg',0.5,true),
+// track('AudioDrop',true,'sounds/drop.wav',0.5,false),
+// track('AudioCard',true,'sounds/card.wav',0.5,false)
+// ];
