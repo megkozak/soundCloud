819c86290ed544e03a99bccd3c87a0c843bdcd3d
Meg Kozak <megkozak@gmail.com>
2017-04-02 10:55:22 -0400

    object added

--- js/main.js
+++ js/main.js
@@ -1,35 +1,36 @@
-function JukeBox(){
+function JukeBox() {
+  // this.play
 }
 
-JukeBox.play = function(){
-  $("#play").click(
-      function() {
-          document.getElementById('player').play();
+JukeBox.prototype.start = function() {
+    $("#play").click(
+        function() {
+            document.getElementById('player').play();
 
-      }
-  );
-  $("#pause").click(
-      function() {
-          document.getElementById('player').pause();
+        }
+    );
+    $("#pause").click(
+        function() {
+            document.getElementById('player').pause();
 
 
-      }
-  );
-  var songs = [
-    "mp3/U2 - With Or Without You.mp3",
-   "mp3/Imagine Dragons - Believer (Audio) copy.mp3",
-   "mp3/James Arthur - Say You Won't Let Go.mp3"
-  ];
-  //
-  $("#nextSong").click(
-    function() {
-      var player = document.getElementById('player');
-      player.src = _.sample(songs);
-      player.play();
-    }
-  );
+        }
+    );
+    var songs = [
+        "mp3/U2 - With Or Without You.mp3",
+        "mp3/Imagine Dragons - Believer (Audio) copy.mp3",
+        "mp3/James Arthur - Say You Won't Let Go.mp3"
+        ];
+    //
+    $("#nextSong").click(
+        function() {
+            var player = document.getElementById('player');
+            player.src = _.sample(songs);
+            player.play();
+        }
+    );
 
 };
 
-// myJukeBox = new JukeBox();
-JukeBox.play();
+myJukeBox = new JukeBox();
+myJukeBox.start();
