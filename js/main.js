57a9c7f7b6a19ef756730d3f2f4683913beee2e6
Meg Kozak <megkozak@gmail.com>
2017-04-02 11:17:28 -0400

    Refactored the object oriented code.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,35 @@
 function JukeBox() {
-  // this.play
+  this.play = function() {
+    this.player().play();
+  }
+  this.pause = function() {
+      this.player().pause();
+  }
+  this.nextSong = function() {
+      var player = this.player();
+      player.src = _.sample(this.songs);
+      player.play();
+  }
+  this.player = function() {
+    return(document.getElementById('player'));
+  }
+  this.songs = [
+      "mp3/U2 - With Or Without You.mp3",
+      "mp3/Imagine Dragons - Believer (Audio) copy.mp3",
+      "mp3/James Arthur - Say You Won't Let Go.mp3"
+      ];
 }
 
-JukeBox.prototype.start = function() {
-    $("#play").click(
-        function() {
-            document.getElementById('player').play();
-
-        }
-    );
-    $("#pause").click(
-        function() {
-            document.getElementById('player').pause();
-
-
-        }
-    );
-    var songs = [
-        "mp3/U2 - With Or Without You.mp3",
-        "mp3/Imagine Dragons - Believer (Audio) copy.mp3",
-        "mp3/James Arthur - Say You Won't Let Go.mp3"
-        ];
-    //
-    $("#nextSong").click(
-        function() {
-            var player = document.getElementById('player');
-            player.src = _.sample(songs);
-            player.play();
-        }
-    );
-
-};
-
 myJukeBox = new JukeBox();
-myJukeBox.start();
+
+$("#play").click(function() {
+  myJukeBox.play();
+});
+
+$("#pause").click(function() {
+  myJukeBox.pause();
+});
+
+$("#nextSong").click(function() {
+  myJukeBox.nextSong();
+});
