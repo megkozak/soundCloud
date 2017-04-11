3df9ac11562ed2b79c13a771365b76b443f4f4bf
Meg Kozak <megkozak@gmail.com>
2017-04-11 11:33:36 -0400

    fixed html tags

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
+// SC.initialize({   client_id: '[fd4e76fc67798bfa742089ed619084a6]' });
+
+// // stream track id 293
+// SC.stream('/tracks/293').then(function(player){
+//   player.play();
+// });
+
 function JukeBox() {
   this.play = function() {
     if (this.player().currentSrc == '')
       this.playSong(this.songs()[0]);
     else
       this.player().play();
-  }
+  };
   this.pause = function() {
       this.player().pause();
-  }
+  };
   this.shuffle = function() {
     this.playSong(_.sample(this.songs()));
-  }
+  };
   this.player = function() {
     return(document.getElementById('player'));
-  }
+  };
   this.songs = function() {
     var result = [];
 
@@ -22,7 +29,7 @@ function JukeBox() {
     });
 
     return(result);
-  }
+  };
   this.chooseSong = function(){
     // var player = this.player();
     // player.src = document.getElementById('songs').value;
@@ -35,12 +42,12 @@ function JukeBox() {
     // console.log(choice);
     // player.src = choice.target.value;
     // console.log(document.getElementById('songs').value);
-  }
+  };
   this.playSong = function(src) {
     var player = this.player();
     player.src = src;
     player.play();
-  }
+  };
 }
 
 myJukeBox = new JukeBox();
