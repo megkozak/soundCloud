8d748e6845139fba5aeb74f55d060b59b19547cb
Meg Kozak <megkozak@gmail.com>
2017-04-12 21:51:12 -0400

    play, pause, shuffle working

--- js/main.js
+++ js/main.js
@@ -1,35 +1,77 @@
-// SC.initialize({   client_id: '[fd4e76fc67798bfa742089ed619084a6]' });
+SC.initialize({   client_id: 'fd4e76fc67798bfa742089ed619084a6' });
 
-// // stream track id 293
-// SC.stream('/tracks/293').then(function(player){
-//   player.play();
+// var tracks = [
+//     '/tracks/238277971',
+//     '/tracks/178747223',
+//     '/tracks/201564439',
+//     '/tracks/246726977'
+//   ];
+
+// var SoundCloud = {}
+// SC.stream().then(function(player){
+//   tracks.push(player);
+//   console.log(tracks);
 // });
 
+// SC.get("/tracks", function(response) {
+//   for (var i = 0; i < response.length; i++) {
+//     $("ul").append("<li>" + response[i].title + "</li>");
+//   }
+// });
+
+// for(var i=0;i <tracks.length; i++){
+//   SC.get(tracks[i]).then(function(response) {
+//     SoundCloud[response.id] = response;
+//   });
+// }
+//
+// for(var i=0;i <tracks.length; i++){
+//   SC.stream(tracks[i]).then(function(response) {
+//     console.log(response);
+//     trackAudio.push(response);
+//   });
+// }
+
 function JukeBox() {
-  this.play = function() {
-    if (this.player().currentSrc == '')
-      this.playSong(this.songs()[0]);
-    else
-      this.player().play();
+  this.start = function() {
+    this.play(0);
+  };
+
+  this.play = function(trackID) {
+    var potato = this;
+    SC.stream(this.songs()[trackID]).then(function(player) {
+      potato.player = player;
+      potato.player.play()
+    });
   };
   this.pause = function() {
-      this.player().pause();
+      this.player.pause();
   };
+
   this.shuffle = function() {
-    this.playSong(_.sample(this.songs()));
+    var randomTrackID = Math.floor(Math.random() * this.songs().length);
+    this.play(randomTrackID);
+
+    // SC.stream(randomTrackID).then(function() {
+    //   // random.player = player;
+    //   // random.player.play()
+    //   this.playSong(_.sample(this.songs()));
+    // });
   };
-  this.player = function() {
-    return(document.getElementById('player'));
-  };
+    // this.playSong(_.sample(this.songs()));
+
+
   this.songs = function() {
-    var result = [];
-
-    $('#songs option').each(function() {
-      result.push(this.value);
-    });
-
+    var result = [
+        '/tracks/238277971',
+        '/tracks/178747223',
+        '/tracks/201564439',
+        '/tracks/246726977'
+      ];
     return(result);
+    };
   };
+
   this.chooseSong = function(){
     // var player = this.player();
     // player.src = document.getElementById('songs').value;
@@ -43,17 +85,19 @@ function JukeBox() {
     // player.src = choice.target.value;
     // console.log(document.getElementById('songs').value);
   };
+
+  // here
   this.playSong = function(src) {
     var player = this.player();
-    player.src = src;
+    player = trackID;
     player.play();
   };
-}
+
 
 myJukeBox = new JukeBox();
 
 $("#play").click(function() {
-  myJukeBox.play();
+  myJukeBox.start();
 });
 
 $("#pause").click(function() {
